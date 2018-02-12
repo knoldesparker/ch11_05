5c72c1b9b8d921a9332cd4a962bca78c2c76b7bb
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-12 10:23:36 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Integer elements[]={7,4,-9,4,15,8,27,7,11,-5,32,-9,-9};
+        List<Integer>  integerList = new LinkedList<>(Arrays.asList(elements));
+
+        System.out.println(integerList);
+        sortAndRemoveDubes(integerList);
+    }
+
+    public static void sortAndRemoveDubes(List<Integer> list){
+        Set<Integer> newSet = new TreeSet<>();
+
+
+        newSet.addAll(list);
+
+
+
+        System.out.println(newSet);
+
+
+    }
 }
